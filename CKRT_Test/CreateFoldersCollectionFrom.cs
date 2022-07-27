63dd57769525eae2fc5a1627f1b6059d9cf0d0b1
renicap <vladimir.dvp3@live.com>
2022-07-27 11:31:48 +0300

    Create CreateFoldersCollectionFrom.cs

---
+++ CKRT_Test/CreateFoldersCollectionFrom.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace CKRT_Test
+{
+    public partial class MainWindow : Window
+    {
+        void CreateFoldersCollectionFrom(FileNode? sourceNode)
+        {
+            if (sourceNode == null) { return; }
+            FolderNodes.Clear();
+            // Сначало папки 
+            foreach (var node in sourceNode.NodesAvailable)
+            {
+                if (node.Type == constDdirectory || node.Type == constParent)
+                {
+                    FolderNodes.Add(node);
+                }
+            }
+            // Потом файлы
+            foreach (var node in sourceNode.NodesAvailable)
+            {
+                if (node.Type == constFile)
+                {
+                    FolderNodes.Add(node);
+                }
+            }
+        }
+    }
+}
