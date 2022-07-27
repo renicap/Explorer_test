212a20b88edf9f8f1579e2d422bf49f2e33814c7
renicap <vladimir.dvp3@live.com>
2022-07-27 11:31:58 +0300

    Create GoingByNodes.cs

---
+++ CKRT_Test/GoingByNodes.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Nodes;
+using System.Windows;
+
+namespace CKRT_Test
+{
+    public partial class MainWindow : Window
+    {
+        static void GoingByNodes(JsonNode? jNode, FileNode? fileNode)
+        {
+            if (jNode == null || fileNode == null) { return; }
+
+            FileNode newNode;
+            string type;
+            if (jNode is JsonArray)
+            {
+                JsonArray jArrayNodes = jNode.AsArray();
+
+                // Добавляем родительскую папку
+                foreach (var jn in jArrayNodes)
+                {
+                    type = (string)jn[constType];
+                    if (type != constParent && fileNode != rootFileNode)      
+                    {
+                        newNode = new FileNode(constParent, "..", fileNode);
+                        fileNode.NodesAvailable.Add(newNode);
+                        break;
+                    }
+                }
+
+                // Добавить папки
+                foreach (var jn in jArrayNodes)
+                {
+                    type = (string)jn[constType];
+                    newNode = new FileNode(type, (string)jn[constName], fileNode);
+                    fileNode.NodesAvailable.Add(newNode);
+                    if (type == constDdirectory)
+                    {
+                        JsonNode jNextNode = jn[constContents];
+                        GoingByNodes(jNextNode, newNode);
+                    }
+                }
+            }
+        }
+    }
+}
