dc6fa585076597dca893681896acda2248bf82f8
renicap <vladimir.dvp3@live.com>
2022-07-27 11:32:11 +0300

    Create MessageWindow.xaml.cs

---
+++ CKRT_Test/MessageWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CKRT_Test
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        string _message = string.Empty;
+        public MessageWindow(string message)
+        {
+            InitializeComponent();
+
+            _message = message;
+            TextBlock_Message.Text = _message;
+        }
+    }
+}
