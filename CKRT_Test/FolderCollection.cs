a9a29291db375dda4620b1a330b80e2b3ad64128
renicap <vladimir.dvp3@live.com>
2022-07-27 11:31:55 +0300

    Create FolderCollection.cs

---
+++ CKRT_Test/FolderCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CKRT_Test
+{
+    internal class FolderCollection : ObservableCollection<FileNode>
+    {
+        public FolderCollection() : base()
+        {
+        }
+    }
+
+}
